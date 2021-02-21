615aa48374af9f2744bc828205a5af658aa6c3aa
Anshul Jangra <anshuljangra2000@hotmail.com>
2021-02-21 15:13:34 +1000

    Initial Commit

--- server/app.js
+++ server/app.js
@@ -1,27 +1,29 @@
-const express = require('express');
-const twitterRouter = require('./routes/twitter');
-var fs =  require('fs');
-var https = require('https');
+const express = require("express");
+const twitterRouter = require("./routes/twitter");
+var fs = require("fs");
+var https = require("https");
 const app = express();
-
-const hostname = '127.0.0.1';
+const hostname = "127.0.0.1";
 const port = 3005;
-const cors = require('cors');
-app.get('/home', (req, res) => {
- const str = '<!DOCTYPE html>' +
-        '<html><head><title>GCTweet Server</title></head>' +
-        '<body>' +
-            '<h1>' + `You shouldn't be here.` + '</h1>' +
-            
-        '</body></html>';
+const cors = require("cors");
 
-    res.writeHead(200,{'content-type': 'text/html'});
-    res.write(str);
-    res.end();
+app.get("/", (req, res) => {
+  const str =
+    "<!DOCTYPE html>" +
+    "<html><head><title>GCTweet Server</title></head>" +
+    "<body>" +
+    "<h1>" +
+    `Server is running.` +
+    "</h1>" +
+    "</body></html>";
+
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write(str);
+  res.end();
 });
 
 app.use(cors());
-app.use('/',twitterRouter);
+app.use("/t-router", twitterRouter);
 
 // http stuff
 // app.listen(port, function () {
@@ -29,13 +31,14 @@ app.use('/',twitterRouter);
 // });
 
 // https stuff
-https.createServer(
+https
+  .createServer(
     {
       key: fs.readFileSync("server.key"),
-      cert: fs.readFileSync("server.cert")
+      cert: fs.readFileSync("server.cert"),
     },
     app
   )
-  .listen(port, function() {
+  .listen(process.env.PROT || port, function () {
     console.log(`Express app listening at https://${hostname}:${port}/`);
   });
--- server/routes/twitter.js
+++ server/routes/twitter.js
@@ -1,58 +1,99 @@
-const Twitter = require('twitter');
-const config = require('../config');
-const express = require('express');
-const axios = require('axios');
-const logger = require('morgan');
+const Twitter = require("twitter");
+const config = require("../config");
+const express = require("express");
+const axios = require("axios");
+const logger = require("morgan");
 const router = express.Router();
 const T = new Twitter(config);
 
-router.use(logger('tiny'));
-router.get('/search', (req, res) => {
-    const options = createTwitterOptions(req.query.query, req.query.result_type, req.query.count, req.query.geocode);
-    
-    T.get('search/tweets', options, function(err, data, response) {
-        if(!err){
-        var tweetData = data.statuses.map(status => {
-            return {
-                id: status.id,
-                text: status.text,
-                userId: status.user.id,
-                userName: status.user.name,
-                screenName: status.user.screen_name,
-                location: status.user.location,
-                profileImage: status.user.profile_image_url_https,
-                geo: status.geo,
-                coordinates: status.coordinates
-            }
+router.use(logger("tiny"));
+router.get("/search", (req, res) => {
+  const options = createTwitterOptions(
+    req.query.query,
+    req.query.result_type,
+    req.query.count,
+    req.query.geocode
+  );
 
-        });
+  T.get("search/tweets", options, function (err, data, response) {
+    if (!err) {
+      var tweetData = data.statuses.map((status) => {
+        return {
+          id: status.id,
+          text: status.text,
+          userId: status.user.id,
+          userName: status.user.name,
+          screenName: status.user.screen_name,
+          location: status.user.location,
+          profileImage: status.user.profile_image_url_https,
+          geo: status.geo,
+          coordinates: status.coordinates,
+        };
+      });
 
-        res.json(tweetData);
-        console.log(tweetData);
-        } else {
-        console.log(err);
+      res.json(tweetData);
+    } else {
+      res.json(err);
+    }
+  });
+});
+
+router.get("/woeid", (req, res) => {
+  const options = createWoeidTwitterOptions(req.query.lat, req.query.long);
+  T.get("trends/closest", options, function (err, data, response) {
+    if (!err) {
+      res.json(data[0].woeid);
+    } else {
+      res.json(err);
+    }
+  });
+});
+
+router.get("/trends", (req, res) => {
+  const options = createTrendTwitterOptions(req.query.id);
+  T.get("trends/place", options, function (err, data, response) {
+    if (!err) {
+      var trends = data[0].trends.map((trend) => {
+        if (trend.tweet_volume != null) {
+          return {
+            name: trend.name,
+            query: trend.query,
+            tweet_volume: trend.tweet_volume,
+          };
         }
-    }) 
-}); 
- 
-
+      });
+      trends = trends.filter((trend) => trend != null);
+      res.json(trends);
+    } else {
+      res.json(err);
+    }
+  });
+});
 // Set up search parameters
 function createTwitterOptions(query, type, tweet_count, geocode) {
-    const params = {
-        q: query,
-        result_type: type,
-        count: tweet_count,
-        lang: 'en',
-        if(geocode){
-            geocode: geocode
-        }
-        
-    }
-    return params;
+  const params = {
+    q: query,
+    result_type: type,
+    count: tweet_count,
+    lang: "en",
+    if(geocode) {
+      geocode: geocode;
+    },
+  };
+  return params;
 }
+function createWoeidTwitterOptions(lat, long) {
+  const params = {
+    lat: lat,
+    long: long,
+  };
+  return params;
+}
+function createTrendTwitterOptions(id) {
+  const params = {
+    id: id,
+  };
+  return params;
+}
+
 module.exports = router;
-
-
-
-
-
